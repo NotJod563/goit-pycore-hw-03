1561e43288d5f12f54a7170e3b2498bd70f3fffb
NotJod563 <chabanovmykola@gmail.com>
2024-07-08 00:24:22 +0300

    Task3_added

---
+++ Task3.py
@@ -0,0 +1,25 @@
+import re
+
+#Cтворення функції яка форматує номери
+def normalize_phone(phone_number):
+    #Видалення всіх символів крім чисел та плюса в рядку phone_numver
+    formatted_number = re.sub(r'[^0-9+]','',phone_number)
+    #Перевірка першого символу чи це початок коду, тобто +
+    if formatted_number[0] != '+':
+        #Створення умови, якщо починається з 380 то додаємо значок +, інакше додаємо код +38
+        if formatted_number.startswith('380'):
+            formatted_number = '+' + formatted_number
+        else:
+            formatted_number = '+38' + formatted_number
+    #функція повертає форматовани номер телефону
+    return formatted_number
+
+
+'''
+#Приклад використання
+print(normalize_phone("+38(050)123-32-34"))  
+print(normalize_phone("0503451234")) 
+print(normalize_phone("(050)8889900"))  
+print(normalize_phone("38050-111-22-22")) 
+print(normalize_phone("38050 111 22 11"))  
+'''
